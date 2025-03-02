55cad45b05f6e2bdf708c249b72e40968eaf8c05
Anna P. <72687297+paramonovaanna@users.noreply.github.com>
2025-03-02 18:34:55 +0300

    model.forward() returns a dict

--- src/models/convnext.py
+++ src/models/convnext.py
@@ -16,5 +16,16 @@ class ConvNeXt_T(nn.Module):
         n_features = self.model.classifier[2].in_features
         self.model.classifier = nn.Linear(n_features, 1)
 
+    def forward(self, img, **batch):
+        """
+        Model forward method.
+
+        Args:
+            img (Tensor): input img.
+        Returns:
+            output (dict): output dict containing logits.
+        """
+        return {"logits": self.model(img)}
+
     def classifier_parameters(self):
         return self.model.classifier.parameters()
