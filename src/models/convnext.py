418f6777e22273b96db343173955a32f71c04562
Anna P. <72687297+paramonovaanna@users.noreply.github.com>
2025-03-02 07:12:26 +0300

    Change last layer of ConvNeXt_T classifier to binary

---
+++ src/models/convnext.py
@@ -0,0 +1,17 @@
+from torchvision.models import convnext_tiny, ConvNeXt_Tiny_Weights
+from torch import nn
+
+
+class ConvNeXt_T(nn.Module):
+
+    def __init__(self, use_pretrained=True):
+        super().__init__()
+
+        if use_pretrained:
+            weights = ConvNeXt_Tiny_Weights.IMAGENET1K_V1
+        else:
+            weights = None
+        self.model = convnext_tiny(weights=weights)
+
+        n_features = self.model.classifier[2].in_features
+        self.model.classifier = nn.Linear(n_features, 1)
