eeac25b95a61521c61e43322ffbd992e38d2848f
Anna P. <72687297+paramonovaanna@users.noreply.github.com>
2025-03-04 18:33:21 +0300

    Add training pipeline

--- train.py
+++ train.py
@@ -5,6 +5,7 @@ import torch
 
 from src.utils.init_utils import set_random_seed, setup_saving_and_logging
 from src.datasets.data_utils import get_dataloaders
+from src.trainer import Trainer
 
 
 @hydra.main(version_base=None, config_path="src/configs", config_name="baseline")
@@ -25,12 +26,30 @@ def main(config):
 
     dataloaders, batch_transforms = get_dataloaders(config, device)
 
-    loss = instantiate(config.loss).to(device)
+    loss_function = instantiate(config.loss).to(device)
     metrics = instantiate(config.metrics)
 
     # freeze layers accept for the last one
     optimizer = instantiate(config.optimizer, params=model.classifier_parameters())
     lr_scheduler = instantiate(config.lr_scheduler, optimizer=optimizer)
+
+    trainer = Trainer(
+        model=model,
+        criterion=loss_function,
+        metrics=metrics,
+        optimizer=optimizer,
+        lr_scheduler=lr_scheduler,
+        config=config,
+        device=device,
+        dataloaders=dataloaders,
+        epoch_len=config.trainer.get("epoch_len"),
+        logger=logger,
+        writer=writer,
+        batch_transforms=batch_transforms,
+        skip_oom=config.trainer.get("skip_oom", True),
+    )
+
+    trainer.train()
 
 if __name__ == "__main__":
     main()
