caed3e3d591b50e36e1b2b5c8931fbe6fe7289c8
Anna P. <72687297+paramonovaanna@users.noreply.github.com>
2025-03-13 09:00:32 +0300

    Use timm models and epoch based training. Change lr_step to 5 and gamma to 0.2

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,1 @@
-from src.models.convnext import ConvNeXt_T
-from src.models.convnext import ConvNeXt_S
-from src.models.convnext import ConvNeXt_B
+from src.models.convnext import ConvNeXt
--- src/models/convnext.py
+++ src/models/convnext.py
@@ -1,19 +1,19 @@
 from torch import nn
 
-from torchvision.models import convnext_tiny, ConvNeXt_Tiny_Weights
-from torchvision.models import convnext_small, ConvNeXt_Small_Weights
-from torchvision.models import convnext_base, ConvNeXt_Base_Weights
+import timm
 
 class ConvNeXt(nn.Module):
 
-    def __init__(self, base_model):
+    def __init__(self, model_name, use_pretrained=True):
         super().__init__()
-        # Change first layer to accept grayscale images
-        self.model = base_model
-        self.model.features[0][0] = nn.Conv2d(1, 96, kernel_size=(4, 4), stride=(4, 4))
+        self.model = timm.create_model(model_name, pretrained=use_pretrained)
 
-        n_features = self.model.classifier[2].in_features
-        self.model.classifier[2] = nn.Linear(n_features, 2)
+        # Change first layer to accept grayscale images
+        self.model.stem[0] = nn.Conv2d(1, 96, kernel_size=(4, 4), stride=(4, 4))
+
+        # Change classifier to classify into genuine and forged
+        n_features = self.model.head.fc.in_features
+        self.model.head.fc = nn.Linear(in_features=n_features, out_features=2, bias=True)
 
     def forward(self, img, **batch):
         """
@@ -25,46 +25,3 @@ class ConvNeXt(nn.Module):
             output (dict): output dict containing logits.
         """
         return {"logits": self.model(img)}
-    
-    def classifier_parameters(self):
-        for param in self.model.parameters():
-            param.requires_grad = False
-        for param in self.model.classifier.parameters():
-            param.requires_grad = True
-        return self.model.classifier.parameters()
-
-
-class ConvNeXt_T(ConvNeXt):
-
-    def __init__(self, use_pretrained=True):
-
-        weights = None
-        if use_pretrained:
-            weights = ConvNeXt_Tiny_Weights.IMAGENET1K_V1
-        model = convnext_tiny(weights=weights)
-
-        super().__init__(model)
-
-
-class ConvNeXt_S(ConvNeXt):
-
-    def __init__(self, use_pretrained=True):
-
-        weights = None
-        if use_pretrained:
-            weights = ConvNeXt_Small_Weights.IMAGENET1K_V1
-        model = convnext_small(weights=weights)
-
-        super().__init__(model)
-
-
-class ConvNeXt_B(ConvNeXt):
-
-    def __init__(self, use_pretrained=True):
-
-        weights = None
-        if use_pretrained:
-            weights = ConvNeXt_Base_Weights.IMAGENET1K_V1
-        model = convnext_base(weights=weights)
-
-        super().__init__(model)
