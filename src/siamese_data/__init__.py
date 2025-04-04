e3b1c3252af19ab00a0fb49ef697242f0d2b97b2
paramonovaanna <paramonovaanna2005@gmail.com>
2025-04-04 23:44:54 +0300

    Delete siamese trainer (integrated into trainer now)

--- src/siamese_data/__init__.py
+++
@@ -1,4 +0,0 @@
-from src.siamese_data.datasets import SiameseTrainDataset, SiameseTestDataset
-from src.siamese_data.data_utils import get_inference_dataloaders, get_dataloaders
-
-from src.siamese_data.collate_fn import train_collate_fn, test_collate_fn
